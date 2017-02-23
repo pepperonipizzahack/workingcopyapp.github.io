b885b5a7bc179184e977c70b1d0c3d4f7f816ffc
Anders Borum <anders@algoritmer.dk>
2017-02-23 09:13:57 +0100

    better extraction of summary and data

--- tools/build-manual.sh
+++ tools/build-manual.sh
@@ -10,7 +10,15 @@ sections=`grep -E < $file 'id="' | awk -Fid= '{print $2}' | awk -F'"' '{print $2
 
 for section in $sections; do
     echo "$section:"
-    markup=`grep -E -A40 < $file "id=\"$section\""`
+    markup=`grep -E -A80 < $file "id=\"$section\""`
+
+	# only grab until next section
+    markup=`echo "$markup" | sed -n -e "/<h[1-4][^>]* id=\"$section/,/<h[1-4] id=/p"`
+	
+	# filter out <pre> blocks
+    markup=`echo "$markup" | sed -e "/<pre>/,/<\/pre>/d"`
+	
+	#echo "$markup"
 
 	# when things are working we need to output this markup
 	# echo "$markup"
@@ -18,12 +26,12 @@ for section in $sections; do
 	
 	# extract title
 	title=`echo "$markup" | grep -E '<h[1-4] ' | head -n 1 | sed -e 's/<[^>]*>//g' | sed -e 's/^ *//'`
-	title=`echo "$title" | sed -e 's/^[ 0-9.]*//'`
+	title=`echo "$title" | sed -e 's/^[ 0-9.]*//' | sed -e 's/&[^;]*;//g'` # remove section number and html entities
 	echo title="$title"
 	
 	# extract summary
 	summary=`echo "$markup" | sed -n "/<p>/,/<\/p>/p" | sed -e 's/<[^>]*>//g' | sed -e 's/^ *//' | tr '\n' ' ' | sed -e 's/ +/ /g' | cut -d ' ' -f 1-250 `
-	summary="$summary..."
+	summary=`echo "$summary" | sed -e 's/["\/]//g' ` # remove qoutes and slashes
 	echo "$summary"
 	
 	# extract image
@@ -44,8 +52,8 @@ for section in $sections; do
 	#  <meta name="twitter:image" content="https://WorkingCopyApp.com/img/action-shot-71.jpg">
 	#  <meta property="og:description" content="Learn how to use this powerful Git client for iOS, for ...">
 	#  <meta name="twitter:description" content="Learn how to use this powerful Git client for iOS, for ...">
-	content=`cat $file | sed -e "s/<title>[^<]*<\/title>/<title>Working Copy - Users Guide - $title<\/title>/"`
-	content=`echo "$content" | sed -e "s/<meta name=\"twitter:title\" [^>]*>/<meta name=\"twitter:title\" content=\"Working Copy - Users Guide - $title\">/"`
+	content=`cat $file | sed -e "s/<title>[^<]*<\/title>/<title>Working Copy - Users Guide - $title<\/title>/"` || exit
+	content=`echo "$content" | sed -e "s/<meta name=\"twitter:title\" [^>]*>/<meta name=\"twitter:title\" content=\"Working Copy - Users Guide - $title\">/"` || exit
 	
 	# replace image when valid
 	if [[ ! -z "$image" ]]; then
@@ -53,13 +61,13 @@ for section in $sections; do
 		echo "image=$imgsrc"
 				
 		# we use ! as regular expression delimiter intead of /
-		content=`echo "$content" | sed -e "s!<meta property=\"og:image\" [^>]*>!<meta property=\"og:image\" content=\"$imgsrc\">!"`
-		content=`echo "$content" | sed -e "s!<meta name=\"twitter:image\" [^>]*>!<meta name=\"twitter:image\" content=\"$imgsrc\">!"`
+		content=`echo "$content" | sed -e "s!<meta property=\"og:image\" [^>]*>!<meta property=\"og:image\" content=\"$imgsrc\">!"` || exit
+		content=`echo "$content" | sed -e "s!<meta name=\"twitter:image\" [^>]*>!<meta name=\"twitter:image\" content=\"$imgsrc\">!"` || exit
 	fi
 	
 	# replace description
-	content=`echo "$content" | sed -e "s/<meta property=\"og:description\" [^>]*>/<meta property=\"og:description\" content=\"$summary\">/"`
-	content=`echo "$content" | sed -e "s/<meta name=\"twitter:description\" [^>]*>/<meta name=\"twitter:description\" content=\"$summary\">/"`
+	content=`echo "$content" | sed -e "s/<meta property=\"og:description\" [^>]*>/<meta property=\"og:description\" content=\"$summary\">/"` || exit
+	content=`echo "$content" | sed -e "s/<meta name=\"twitter:description\" [^>]*>/<meta name=\"twitter:description\" content=\"$summary\">/"` || exit
 		
 	# write to file
 	mkdir -p "$base/manual/$section"
